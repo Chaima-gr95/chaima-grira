106bc1612e5059e5558c562266a5a3395e0b0ab0
Chaima Grira <grirachaima302@gmail.com>
2025-02-23 17:49:36 +0100

    Add files via upload

---
+++ TP2.java
@@ -0,0 +1,19 @@
+class ServicesImpression {
+    imprimer (int(a));
+}
+class pc {
+    void impression(ServicesImpression)
+}
+class ImprimenterLaser {
+    void imprimer ();
+}
+class ImprimenterJetEncore{
+    class implements ServicesImpression{
+        void imprimer ();
+    }
+}
+
+public class TestInterfaxcePCImprimante {
+    pc1 = new pc;
+    pc2= new pc;
+}
